510de9fd2b8ec1c540d17bc3cff6b93557024e9e
C1 Muhamad Regen <180326@maxxis.id>
2018-09-24 09:11:31 +0700

    template

---
+++ application/config/admin/dp_config.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['title']      = 'BoGOR';
+$config['title_mini'] = 'Bo';
+$config['title_lg']   = 'GOR';
+$config['title_des'] = 'BOOKING';
+
+
+
+/* Display panel login */
+$config['auth_social_network'] = TRUE;
+$config['forgot_password']     = TRUE;
+$config['new_membership']      = TRUE;
+
+
+
+/*
+ * **********************
+ * AdminLTE
+ * **********************
+ */
+/* Page Title */
+$config['pagetitle_open']     = '<h1>';
+$config['pagetitle_close']    = '</h1>';
+$config['pagetitle_el_open']  = '<small>';
+$config['pagetitle_el_close'] = '</small>';
+
+/* Breadcrumbs */
+$config['breadcrumb_open']     = '<ol class="breadcrumb">';
+$config['breadcrumb_close']    = '</ol>';
+$config['breadcrumb_el_open']  = '<li>';
+$config['breadcrumb_el_close'] = '</li>';
+$config['breadcrumb_el_first'] = '<i class="fa fa-dashboard"></i>';
+$config['breadcrumb_el_last']  = '<li class="active">';
+
+
